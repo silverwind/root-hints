c6bedd48158cf92e131a049f19a5ad1736d120c4
silverwind <me@silverwind.io>
2015-01-31 18:46:49 +0100

    Add tests, update package and licence

---
+++ test.js
@@ -0,0 +1,37 @@
+/* global it */
+"use strict";
+
+var assert = require("assert");
+var hints  = require("./");
+var ipv4regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+var ipv6regex = /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))\s*$/;
+
+it("should return an array", function () {
+    assert(Array.isArray(hints("A")));
+    assert(Array.isArray(hints("AAAA")));
+    assert(Array.isArray(hints()));
+});
+
+it("should return a minimum of records", function () {
+    assert(hints("A").length >= 13);
+    assert(hints("AAAA").length >= 11);
+    assert(hints().length >= 13);
+});
+
+it("should return valid IPv4 addresses", function () {
+    hints("A").forEach(function (address) {
+        assert(ipv4regex.test(address));
+    });
+});
+
+it("should return valid IPv6 addresses", function () {
+    hints("AAAA").forEach(function (address) {
+        assert(ipv6regex.test(address));
+    });
+});
+
+it("should return names in the root-servers.net zone", function () {
+    hints().forEach(function (hint) {
+        assert(/^[a-z]\.root-servers\.net$/.test(hint.name));
+    });
+});
