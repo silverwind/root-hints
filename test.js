84678f9f52e8e3daa63b762a138b646939d5db4f
silverwind <me@silverwind.io>
2017-03-20 20:54:07 +0100

    add travis, simplify tests

--- test.js
+++ test.js
@@ -2,34 +2,24 @@
 
 var assert = require("assert");
 var net    = require("net");
-var hints  = require("./");
+var hints  = require(".");
 
-it("should return an array", function() {
-  assert(Array.isArray(hints("A")));
-  assert(Array.isArray(hints("AAAA")));
-  assert(Array.isArray(hints()));
+assert(Array.isArray(hints("A")));
+assert(Array.isArray(hints("AAAA")));
+assert(Array.isArray(hints()));
+
+assert(hints("A").length >= 13);
+assert(hints("AAAA").length >= 13);
+assert(hints().length >= 13);
+
+hints("A").forEach(function(address) {
+  assert(net.isIPv4(address));
 });
 
-it("should return a minimum of records", function() {
-  assert(hints("A").length >= 13);
-  assert(hints("AAAA").length >= 11);
-  assert(hints().length >= 13);
+hints("AAAA").forEach(function(address) {
+  assert(net.isIPv6(address));
 });
 
-it("should return valid IPv4 addresses", function() {
-  hints("A").forEach(function(address) {
-    assert(net.isIPv4(address));
-  });
-});
-
-it("should return valid IPv6 addresses", function() {
-  hints("AAAA").forEach(function(address) {
-    assert(net.isIPv6(address));
-  });
-});
-
-it("should return names in the root-servers.net zone", function() {
-  hints().forEach(function(hint) {
-    assert(/^[a-z]\.root-servers\.net$/.test(hint.name));
-  });
+hints().forEach(function(hint) {
+  assert(/^[a-z]\.root-servers\.net$/.test(hint.name));
 });
